7243b43f5eeca9440a41080e80b9d15bd6ba47fc
Peter-McKinney <45462076+Peter-McKinney@users.noreply.github.com>
2023-10-15 01:15:45 -0500

    Create output, fixup lines.

--- index.js
+++ index.js
@@ -2,53 +2,39 @@ import fs from 'fs';
 import path from 'path';
 import { createCanvas } from 'canvas';
 
-const columnsToRemove = ["Alias 1", "Alias 2", "Full Name"];
 const csvData = fs.readFileSync('./files/tavern-keep-roster.csv', 'utf-8');
+const rowHeight = 25;
+const columnsToRemove = ['Alias 1', 'Alias 2', 'Full Name'];
 
+function parseCSVtoArray(csvData, columnsToRemove) {
+  let rows = csvData
+    .trim()
+    .split('\n')
+    .map((row) => row.split(','));
 
-function parseCSVtoArray(csvData) {
-  const rows = csvData.trim().split('\n').map(row => row.split(','));
-  const indicesToRemove = rows[0].map((colName, index) => columnsToRemove.includes(colName) ? index : -1).filter(index => index !== -1);
-  const filteredRows = rows.map(row => row.filter((_, index) => !indicesToRemove.includes(index)));
-  return filteredRows;
+  if (columnsToRemove) {
+    const indicesToRemove = rows[0]
+      .map((colName, index) =>
+        columnsToRemove.includes(colName) ? index : undefined
+      )
+      .filter((index) => index);
+
+    rows = rows.map((row) =>
+      row.filter((_, index) => !indicesToRemove.includes(index))
+    );
+  }
+
+  return rows;
 }
 
-function renderCSVToCanvasColWidth(rows, canvas) {
-  const rowHeight = 25;
-  const context = canvas.getContext('2d');
-  context.font = '16px Arial';
-  context.fillStyle = 'white';
-  context.fillRect(0, 0, canvas.width, canvas.height);
-
-  context.fillStyle = 'black'
-
-  const colWidths = rows[0].map((_, colIndex) =>
-    Math.max(...rows.map(row => context.measureText(row[colIndex]).width))
-  );
-
-  const cellPadding = 10;
-  const cellHeight = 30;
-  const cellWidth = 100;
-
-  // let y = 20;  // Starting Y position
-  // rows.forEach(row => {
-  //   let x = 0;  // Starting X position for each row
-  //   row.forEach((cell, colIndex) => {
-  //     context.fillText(cell, x, y);
-  //     x += colWidths[colIndex] + 10;  // Move X by the column width + some padding
-  //   });
-  //   y += 20;  // Move to the next line (adjust as needed for cell height)
-  // });
-
-
-  let y = 20;  // Starting Y position
+function drawHorizontalLines(rows, context, colWidths) {
+  let y = 20; // Starting Y position
+  let finalX = 0;
   rows.forEach((row, rowIndex) => {
     let x = 0;
     row.forEach((cell, colIndex) => {
-
       // Draw cell text
-      context.fillText(cell, x + 5, y + (rowHeight / 2) + 5);
-
+      context.fillText(cell, x + 5, y + rowHeight / 2 + 5);
       x += colWidths[colIndex] + 10;
     });
 
@@ -58,21 +44,49 @@ function renderCSVToCanvasColWidth(rows, canvas) {
     context.stroke();
 
     y += rowHeight;
+    finalX = x;
   });
 
+  //draw the final horizontal line
+  context.moveTo(0, y);
+  context.lineTo(finalX, y);
+  context.stroke();
+
+  return y;
+}
+
+function drawVerticalLines(context, colWidths, y) {
   // Draw vertical lines
   let x = 0;
-  colWidths.forEach(width => {
-    context.moveTo(x, 20 - rowHeight + 8);  // Assuming 20 is starting Y as in previous example
-    context.lineTo(x, y);  // 'y' is now at the bottom of the table after all rows
+  colWidths.forEach((width) => {
+    context.moveTo(x, 20 - rowHeight + 24); // Assuming 20 is starting Y as in previous example
+    context.lineTo(x, y); // 'y' is now at the bottom of the table after all rows
     context.stroke();
     x += width + 10;
   });
 
   // Draw the final vertical line for the table's rightmost boundary
-  context.moveTo(x, 20 - rowHeight + 5);
+  context.moveTo(x, 20 - rowHeight + 24);
   context.lineTo(x, y);
   context.stroke();
+}
+
+function renderCSVToCanvasPredictiveColWidth(rows, canvas) {
+  const context = canvas.getContext('2d');
+  context.font = '16px Arial';
+  context.fillStyle = 'white';
+  context.fillRect(0, 0, canvas.width, canvas.height);
+
+  context.fillStyle = 'black';
+
+  //we need to get the maximum column widths so we can set the cell width
+  //to the largest colWidth in the column.
+  const colWidths = rows[0].map((_, colIndex) =>
+    Math.max(...rows.map((row) => context.measureText(row[colIndex]).width))
+  );
+
+  let y = drawHorizontalLines(rows, context, colWidths);
+  drawVerticalLines(context, colWidths, y);
 }
 
 function createPNGFileFromCanvas(canvas) {
@@ -82,7 +96,7 @@ function createPNGFileFromCanvas(canvas) {
 }
 
 const canvas = createCanvas(900, 900);
-renderCSVToCanvasColWidth(parseCSVtoArray(csvData), canvas);
+renderCSVToCanvasPredictiveColWidth(parseCSVtoArray(csvData, columnsToRemove), canvas);
 createPNGFileFromCanvas(canvas);
 
 const dataURL = canvas.toDataURL();
--- scripts/replace-data-url.js
+++ scripts/replace-data-url.js
@@ -1,11 +1,20 @@
 import fs from 'fs';
 import path from 'path';
 
-
-const dataUrl = fs.readFileSync(path.join(process.cwd(), 'data-url.txt'), 'utf-8');
-let htmlData = fs.readFileSync(path.join(process.cwd(), './html/index.html'), 'utf-8');
+const dataUrl = fs.readFileSync(
+  path.join(process.cwd(), './output/data-url.txt'),
+  'utf-8'
+);
+let htmlData = fs.readFileSync(
+  path.join(process.cwd(), './html/index.html'),
+  'utf-8'
+);
 
 const regex = /<img [^>]*>/g;
 htmlData = htmlData.replace(regex, `<img src="${dataUrl}">`);
 
-fs.writeFileSync(path.join(process.cwd(), './html/index.html'), htmlData, 'utf-8');
+fs.writeFileSync(
+  path.join(process.cwd(), './output/index.html'),
+  htmlData,
+  'utf-8'
+);
